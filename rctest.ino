5b826863ec36a68f7cce11fceffa92ec53a9cec5
worldOneo <62426504+worldOneo@users.noreply.github.com>
2021-03-02 22:08:48 +0100

    Added motor controll

--- rctest.ino
+++ rctest.ino
@@ -1,25 +1,6 @@
-//include <makros.h>
-//include <debug.h>
+#include <Servo.h>
 #include "RCReceive.h"
 #include "SparkFunLSM6DS3.h"
-/*
-  RC_Template.ino - Template for RC Receiver enabled programs - Version 0.2
-  Copyright (c) 2012 Wilfried Klaas.  All right reserved.
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 
 uint16_t neutralSTE = 1500;
 uint16_t neutralTHR = 1500;
@@ -32,26 +13,42 @@ const uint16_t HAREA = AREA / 2;
 const byte D9 = 9;
 const byte D10 = 10;
 
-const byte PIN_RC_STE = D9;
-const byte PIN_RC_THR = D10;
+//PIN_CTR = Controll pins for the Motor right and left
+const byte PIN_CTR_R = D9;
+const byte PIN_CTR_L = D10;
+
+const byte D7 = 7;
+const byte D8 = 8;
+
+//PIN_RC = Receiving pins for the Remote
+const byte PIN_RC_STE = D7;
+const byte PIN_RC_THR = D8;
 
 int8_t rotation = 1;
 
 RCReceive steReceive;
 RCReceive thrReceive;
 LSM6DS3 GyroscopeIMU;
+Servo lMotor;
+Servo rMotor;
 
 void setup()
 {
   Serial.begin(9600);
+
   pinMode(PIN_RC_STE, INPUT);
   pinMode(PIN_RC_THR, INPUT);
+
+  lMotor.attach(PIN_CTR_L);
+  rMotor.attach(PIN_CTR_R);
+
   steReceive.attachInt(PIN_RC_STE);
   thrReceive.attachInt(PIN_RC_THR);
 
   GyroscopeIMU.begin();
 
-  // put your setup code here, to run once:
+  lMotor.writeMicroseconds(neutralSTE);
+  rMotor.writeMicroseconds(neutralSTE);
 }
 
 void loop()
@@ -60,7 +57,6 @@ void loop()
   steReceive.poll();
   thrReceive.poll();
 
-  // zero point determination?
   if (steReceive.hasNP() && !steReceive.hasError())
   {
     doWork();
@@ -68,7 +64,6 @@ void loop()
   else if (steReceive.hasError())
   {
     Serial.println("Failed to read RC");
-    // Failure handling failsafe or something ...
   }
 }
 
@@ -89,7 +84,6 @@ void doWork()
     rotation = -1;
   }
 
-  calcMotorRSpeed(ste, thr);
   Serial.print((int)ste);
   Serial.print(" ");
   Serial.print((int)thr);
@@ -98,23 +92,12 @@ void doWork()
   Serial.print(" ");
   Serial.print(calcMotorRSpeed(ste, thr));
   Serial.println();
-  /*Serial.print(" ");
-  Serial.println(calcMotorRSpeed(ste, thr));*/
-  // put your main code here, to run repeatedly:
+  lMotor.writeMicroseconds(calcMotorLSpeed(ste, thr));
+  rMotor.writeMicroseconds(calcMotorRSpeed(ste, thr));
 }
 
 int16_t calcMotorLSpeed(float ste, float thr)
 {
-  /* if (ste < NPT && ste > NMT && thr < NPT && thr > NMT)
-    return 1500;
-
-  float thrfactor;
-  float stefactor;
-
-  thrfactor = ((thr - neutralTHR) / HAREA);
-  stefactor = 1 - ((ste - neutralSTE) / HAREA) * rotation;
-
-  float motor = 1500 + thrfactor * stefactor * HAREA; */
   float motor = calcMotorSpeed(ste, thr, 1);
   if (motor < 1500)
   {
@@ -125,16 +108,6 @@ int16_t calcMotorLSpeed(float ste, float thr)
 
 int16_t calcMotorRSpeed(float ste, float thr)
 {
-  /*if (ste < NPT && ste > NMT && thr < NPT && thr > NMT)
-    return 1500;
-
-  float thrfactor;
-  float stefactor;
-
-  thrfactor = ((thr - neutralTHR) / HAREA);
-  stefactor = 1 - ((ste - neutralSTE) / HAREA) * -rotation;
-
-  float motor = 1500 - thrfactor * stefactor * HAREA;*/
   float motor = calcMotorSpeed(ste, thr, -1);
   if (motor > 1500)
   {
